60420600945c8e42518ed0a0f30695dbcac686cb
tk-955990 <105995713+tk-955990@users.noreply.github.com>
2022-11-20 13:58:16 +0900

    2022/11/20/1358

--- yucatio/Employee.java
+++ yucatio/Employee.java
@@ -1,43 +1,48 @@
 package yucatio;
 
 public class Employee {
-  
-  String name;
-  String deploy;
-  String gender;
-  
-  public Employee(String name,String deploy,String gender) {
-	  this.name = name;
-	  this.deploy = deploy;
-	  this.gender = gender; 
-  }
-  
-  
-  public String getName() {
-	  return name;
-  }
-  public void setName(String name) {
-	  this.name = name;
-  }
-  
-  public String getDeploy() {
-	  return deploy;
-  }
-  public void setDeploy(String deploy) {
-	  this.deploy = deploy;
-  }
-  
-  public String getGender() {
-	  return gender;
-  }
-  public void setGender(String gender) {
-	  this.gender = gender;
-  }
-  
-  
-  @Override
-  public String toString() {
-	  return String.format("名前:%12s 部署:%13s 性別:%s", this.name,this.deploy,this.gender );
-  }
-  
+
+	Employee() {
+
+	}
+
+
+	public Employee(String name,String section,String gender) {
+		this.name = name;
+		this.section = section;
+		this.gender = gender; 
+	}
+
+	private String name;
+	private String section;
+	private String gender;
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSection() {
+		return section;
+	}
+	public void setSection(String section) {
+		this.section = section;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+	@Override
+	public String toString() {
+		return String.format("名前:%-15s 部署:%-15s 性別:%s", this.name,this.section,this.gender );
+	}
+
 }
--- yucatio/MapCount.java
+++ yucatio/MapCount.java
@@ -1,34 +1,66 @@
 package yucatio;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class MapCount {
-  public static void main(String args[])  {
-	// new Employee(名前、部署、性別)
-	  List<Employee> employee = new ArrayList<>();  
-	  
-	  employee.add(new Employee("yucatio"    , "情報システム部"    ,"F")); 
-	  employee.add(new Employee("ハル"       , "情報システム部"    ,"M")); 
-	  employee.add(new Employee("saito_san"  , "情報システム部"    ,"M")); 
-	  employee.add(new Employee("山葵"       , "情報システム部"    ,"F")); 
-	  employee.add(new Employee("けこ"       , "情報システム部"    ,"F")); 
-	  employee.add(new Employee("麻生"       , "情報システム部"    ,"M")); 
-	  employee.add(new Employee("bata"       , "情報システム部"    ,"M")); 
-	  employee.add(new Employee("市川"       , "情報システム部"    ,"M")); 
-	  employee.add(new Employee("ちか"       , "情報システム部"    ,"M")); 
-	  employee.add(new Employee("Hayashi"    , "情報セキュリティー部","M")); 
-	  employee.add(new Employee("astraea"    , "情報セキュリティー部","M")); 
-	  employee.add(new Employee("菅原鮮魚店" , "情報セキュリティー部","M")); 
-	  employee.add(new Employee("M.Pentes"   , "情報セキュリティー部","M")); 
-	  employee.add(new Employee("LOST_CANYON", "登山研究部"        ,"M")); 
-	  employee.add(new Employee("zero"       , "登山研究部"        ,"M")); 
-	  employee.add(new Employee("FLASH"      , "登山研究部"        ,"F")); 
+	public static void main(String args[])  {
+		// new Employee(名前、部署、性別)
+		List<Employee> employee = new ArrayList<>();  
 
-	  
-	  employee.stream().forEach(System.out::println);
-  }
-	
-	
-	
+		employee.add(new Employee("yucatio"    , "情報システム部"      ,"F")); 
+		employee.add(new Employee("ハル"       , "情報システム部"      ,"M")); 
+		employee.add(new Employee("saito_san"  , "情報システム部"      ,"M")); 
+		employee.add(new Employee("山葵"       , "情報システム部"      ,"F")); 
+		employee.add(new Employee("けこ"       , "情報システム部"      ,"F")); 
+		employee.add(new Employee("麻生"       , "情報システム部"      ,"M")); 
+		employee.add(new Employee("bata"       , "情報システム部"      ,"M")); 
+		employee.add(new Employee("市川"       , "情報システム部"      ,"M")); 
+		employee.add(new Employee("ちか"       , "情報システム部"      ,"M")); 
+		employee.add(new Employee("Hayashi"    , "情報セキュリティー部","M")); 
+		employee.add(new Employee("astraea"    , "情報セキュリティー部","M")); 
+		employee.add(new Employee("菅原鮮魚店" , "情報セキュリティー部","M")); 
+		employee.add(new Employee("M.Pentes"   , "情報セキュリティー部","M")); 
+		employee.add(new Employee("LOST_CANYON", "登山研究部"          ,"M")); 
+		employee.add(new Employee("zero"       , "登山研究部"          ,"M")); 
+		employee.add(new Employee("FLASH"      , "登山研究部"          ,"F")); 
+
+
+
+		//    Map<String, Integer> countMap = countBySection(employee)
+
+		Map<String, Integer> countMap = employee
+				.stream()
+				.collect(Collectors.groupingBy(employee::getSection,
+						Collectors.counting()));
+
+		for(Map.Entry<String,Integer>map: countMap.entrySet()) {
+			System.out.println(map.getKey() + " :" + map.getValue());
+		}
+
+
+		/*		Map<String,Integer> countMap  = employee
+				.stream()
+				.collect(Collectors.groupingBy(employee::getSection,
+						Collectors.counting()));
+
+		for(Map.Entry<String,Integer>map: countMap.entrySet()) {
+			System.out.println(map.getKey() + " :" + map.getValue());
+		}
+
+		 */
+
+	}
+
+
 }
+
+
+
+
